304b46db92f3a1867dd9a0a84f1ad3565b447367
Catelen Wu <cw3223@columbia.edu>
2020-10-10 17:42:47 -0400

    update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, request, render_template
 from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        link = request.form['link']
+    if request.method == "POST":
+        link = request.form["link"]
         site = requests.get(link)
         source = site.content
-        soup = BeautifulSoup(source, 'lxml')
-        print(soup)
+        soup = BeautifulSoup(source, "lxml")
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
