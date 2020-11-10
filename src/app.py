588a856b70a58abb7441a4408828ddae83ee8e9f
Catelen Wu <cw3223@columbia.edu>
2020-11-09 19:35:58 -0500

    removed coverage and pycache files

--- src/app.py
+++ src/app.py
@@ -1,28 +1,31 @@
 from flask import Flask, request, render_template
 from bs4 import BeautifulSoup
-import requests
+import requests, sys, importlib
+from urllib.request import Request, urlopen
 
 app = Flask(__name__)
 
+importlib.reload(sys)
+sys.getdefaultencoding()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         link = request.form["link"]
-        site = requests.get(link)
-        source = site.content
+        response = requests.get(link, headers={'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+        source = response.content
         soup = BeautifulSoup(source, "lxml")
-        print(link)
+        print(soup)
         #return "Link " + link
         ingredients = ""
-        tag = soup.find('div', {'class': 'entry clearfix'})
+        tag = soup.find_all('div', {'class': 'entry clearfix'})
         print(tag)
         #ingredients = ingredients + "\n" + tag.find('ul')
         #print(ingredients)
 
     return render_template("index.html")
 @app.route("/link")
-def link_route():
+def link_route(): 
     return render_template("index.html")
 
 if __name__ == "__main__":
