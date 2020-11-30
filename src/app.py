1383482d23af92ec7828f28188af6ad5b9887622
Catelen Wu <cw3223@columbia.edu>
2020-11-29 21:11:51 -0500

    extract ingredients

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from bs4 import BeautifulSoup
 import requests, sys, importlib, re
 from urllib.request import Request, urlopen
@@ -19,6 +19,7 @@ def index():
         #specific to this link at the moment
         #page = requests.get("https://thecinnaman.com/blog/malaweh-with-scallion-cheddar-cheese")
         ingredients = []
+        new_ingredients = []
         page = requests.get(request.form["link"])
         #print(page.content)  
         soup = BeautifulSoup(page.content, 'html.parser')
@@ -41,19 +42,23 @@ def index():
             for token in tokens_without_sw:
                 if len(token) < 3:
                     tokens_without_sw.remove(token)
-            print(tokens_without_sw)
-        return render_template("ingredients.html")
+            #print(tokens_without_sw)
+            tokens_without_sw = " ".join(tokens_without_sw)
+            new_ingredients.append(tokens_without_sw)
+        #return redirect(url_for("ingredients", new_list = new_ingredients, len = len(new_ingredients)))
+        #return redirect(url_for("ingredients", new_list = new_ingredients))
+        return render_template("ingredients.html", new_list = new_ingredients, len = len(new_ingredients))
         # link = request.form["link"] 
         # response = requests.get(link, headers={'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
         # source = response.content
-        # soup = BeautifulSoup(source, "lxml")
+        # soup = BeautifulSoup(source, "lxml") 
         # #return "Link " + link
-
     return render_template("index.html")
 
-@app.route("/ingredients")
-def link_route(): 
-    return render_template("index.html")
-
+""" @app.route("/ingredients")
+def ingredients():
+    print(new_list)
+    return render_template("ingredients.html", new_list = new_list, len = len(new_list)) 
+ """
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
